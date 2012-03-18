c967b48d262324cc8d74239f4d785a9bc0c95205
welldan97 <welldan97@gmail.com>
2012-03-18 19:13:30 +0400

    Fixed migration template

--- lib/generators/protocolist/install/templates/migration.rb
+++ lib/generators/protocolist/install/templates/migration.rb
@@ -1,16 +1,14 @@
 class CreateActivities < ActiveRecord::Migration
   def change
-    def change
-      create_table :activities do |t|
-        t.references :subject, :polymorphic => true
-        t.references :object, :polymorphic => true
-        t.string :type
-        t.text :data
+    create_table :activities do |t|
+      t.references :subject, :polymorphic => true
+      t.references :object, :polymorphic => true
+      t.string :type
+      t.text :data
 
-        t.timestamps
-      end
-      add_index :activities, :subject_id
-      add_index :activities, :object_id
+      t.timestamps
     end
+    add_index :activities, :subject_id
+    add_index :activities, :object_id
   end
 end
