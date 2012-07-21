a2b00e062138f452bcf26127e087c815802d0c3e
Pyotr Koryakin <pyotr.koryakin@ainstainer.com>
2012-07-21 14:24:19 +0400

    Added Mongoid support

--- lib/protocolist/railtie.rb
+++ lib/protocolist/railtie.rb
@@ -4,6 +4,13 @@ module Protocolist
       ActiveSupport.on_load :active_record do
         include ModelAdditions
       end
+      if defined?(Mongoid)
+        Mongoid::Document.module_eval do
+          included do
+            include ModelAdditions
+          end
+        end
+      end
     end
     initializer 'protocolist.controller_additions' do
       ActiveSupport.on_load :action_controller do
