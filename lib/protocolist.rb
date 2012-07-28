c9369dbc24364bffa4f7b0e840cc79819cc5644a
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-07-28 16:12:52 +0200

    refactor Protocolist class accessors

--- lib/protocolist.rb
+++ lib/protocolist.rb
@@ -13,20 +13,8 @@ module Protocolist
     options = { :actor => @actor, :activity_type => activity_type }.merge options
     @activity_class.create options if options[:actor] && @activity_class
   end
-
-  def self.actor
-    @actor
-  end
-
-  def self.actor= actor
-    @actor = actor
-  end
-
-  def self.activity_class
-    @activity_class
-  end
-
-  def self.activity_class= activity_class
-    @activity_class = activity_class
+  
+  class << self
+    attr_accessor :actor, :activity_class
   end
 end
