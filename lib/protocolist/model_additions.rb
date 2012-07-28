479abe96561921b0d3b387f609b0d739c5f15f3e
Krzysztof Zalewski <zlw.zalewski@gmail.com>
2012-07-28 14:43:47 +0200

    refactor ModelAdditions

--- lib/protocolist/model_additions.rb
+++ lib/protocolist/model_additions.rb
@@ -1,41 +1,42 @@
 module Protocolist
   module ModelAdditions
-    module ClassMethods
-      def fires activity_type, options={}
-        #options normalization
-        fires_on = Array(options[:on] || activity_type)
-
-        data_proc = if options[:data].respond_to?(:call)
-                      lambda{|record| options[:data].call(record)}
-                    elsif options[:data].class == Symbol
-                      lambda{|record| record.send(options[:data]) }
-                    else
-                      lambda{|record| options[:data] }
-                    end
-
-        options_for_callback = options.select{|k,v| [:if, :unless].include? k }
-
-        options_for_fire = options.reject{|k,v| [:if, :unless, :on].include? k }
-
-        callback_proc = lambda{|record|
-          record.fire activity_type, options_for_fire.merge({:data => data_proc.call(record)})
-        }
-
-        fires_on.each do |on|
-          send("after_#{on.to_s}".to_sym, callback_proc, options_for_callback)
-        end
-      end
-    end
-
-    def self.included base
-      base.extend ClassMethods
-    end
-
-    def fire activity_type, options={}
+    extend ActiveSupport::Concern
+    
+    def fire(activity_type, options={})
       options[:target] = self if options[:target] == nil
-      options[:target] = nil if options[:target] == false
+      options[:target] = nil  if options[:target] == false
 
       Protocolist.fire activity_type, options
+    end
+    
+    module ClassMethods
+      def fires(activity_type, options={})
+        fires_on  = [*options[:on] || activity_type]
+        data_proc = extract_data_proc options[:data]
+
+        options_for_callback = options.slice(:if, :unless)
+        options_for_fire     = options.except(:if, :unless, :on)
+
+        callback_proc = lambda do |record|
+          record.fire(activity_type, options_for_fire.merge(:data => data_proc.call(record)))
+        end
+
+        fires_on.each do |on|
+          send("after_#{on}", callback_proc, options_for_callback)
+        end
+      end
+      
+      private
+      
+      def extract_data_proc(data)
+        if data.respond_to? :call
+          lambda {|record| data.call record }
+        elsif data.is_a? Symbol
+          lambda {|record| record.send data }
+        else
+          lambda {|record| data }
+        end
+      end
     end
   end
 end
