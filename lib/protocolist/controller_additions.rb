f4cfc13cffeb9ebd5a11cf349f2a3565b3ef0179
Dmitry Yakimov <welldan97@gmail.com>
2012-07-31 12:19:29 +0400

    Refactor controller additions
    Earlier version overrided options in the caller, which is smell.
    This new refactoring fix this behavior and makes code easier to understand

--- lib/protocolist/controller_additions.rb
+++ lib/protocolist/controller_additions.rb
@@ -8,24 +8,34 @@ module Protocolist
     include Util::DataProc
 
     def fire(activity_type = nil, options = {})
-      options[:target] = instance_variable_get("@#{controller_name.singularize}") if options[:target] == nil
-      options[:target] = nil if options[:target] == false
+      target = case options[:target]
+               when nil
+                 instance_variable_get "@#{controller_name.singularize}"
+               when false
+                 nil
+               else
+                 options[:target]
+               end
+
       activity_type ||= action_name.to_sym
+      options = options.merge target: target
 
       Protocolist.fire(activity_type, options)
     end
-    
+
     module ClassMethods
       def fires(activity_type, options = {})
-        options[:only] ||= activity_type unless options[:except]
+        options = options.merge(only: activity_type) unless options[:only] || options[:except]
 
         data_proc = extract_data_proc options[:data]
 
         options_for_callback = options.slice(:if, :unless, :only, :except)
         options_for_fire     = options.except(:if, :unless, :only, :except)
 
-        callback_proc = lambda do |controller|
-          controller.fire(activity_type, options_for_fire.merge(data: data_proc.call(controller)))
+        callback_proc = lambda do |controller; options|
+          options = options_for_fire.merge data: data_proc.call(controller)
+
+          controller.fire activity_type, options
         end
 
         send(:after_filter, callback_proc, options_for_callback)
